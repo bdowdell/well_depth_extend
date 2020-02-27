06062080d8dff14a114ef196d09c5082208fa42b
bdowdell <ben.dowdell87@gmail.com>
2020-02-26 19:45:51 -0600

    Adds functions

--- src/wellDepthExtend.py
+++ src/wellDepthExtend.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env/ python
 
 import os
+import pandas as pd
+import numpy as np
 
 def get_file_list(path=os.getcwd()):
     """This function returns the contents of a given directory
@@ -9,4 +11,34 @@ def get_file_list(path=os.getcwd()):
     Returns a list
     """
     return os.listdir(path)
+
+def pair_well_files(file_list):
+    """This function reads in the contents of a directory
+    And pairs unique well LAS file with well deviation survey
+    Assumes file_list a list
+    Returns a dictionary
+    """
+    pass
+
+def read_well_las(well_file):
+    """This function reads in a LAS file
+    Assumes well_file a string
+    Returns a pandas dataframe
+    """
+    pass
+
+def read_dev_surv(dev_surv_file):
+    """This function reads in a CSV file
+    Assumes dev_surv_file a string
+    Returns a pandas dataframe
+    """
+    pass
+
+def reference_curves(dev_surv, start_depth=0, kb=32, stop_depth=10000, inc=5, units='ft'):
+    """This function builds uniformly spaced depth reference curves
+    Defaults start_depth, kb, stop_depth, inc, and units
+    Assumes dev_surv a pandas dataframe
+    Returns a pandas dataframe
+    """
+    pass
 
