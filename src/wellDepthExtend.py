29f7798933c136308cb1bb415d0261e5f6479ee0
bdowdell <ben.dowdell87@gmail.com>
2020-02-26 19:20:41 -0600

    updates func get_file_list

--- src/wellDepthExtend.py
+++ src/wellDepthExtend.py
@@ -2,9 +2,10 @@
 
 import os
 
-def get_file_list(path):
+def get_file_list(path=os.getcwd()):
     """This function returns the contents of a given directory
     Assumes path a string
+    Defaults path to be current working directory
     Returns a list
     """
     return os.listdir(path)
