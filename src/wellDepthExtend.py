e6637325a879d4025bf9619e68b80daa489a6500
bdowdell <ben.dowdell87@gmail.com>
2020-02-27 01:06:37 -0600

    Finishes func dog_leg_severity

--- src/wellDepthExtend.py
+++ src/wellDepthExtend.py
@@ -34,11 +34,33 @@ def read_dev_surv(dev_surv_file):
     """
     pass
 
-def reference_curves(dev_surv, start_depth=0, kb=32, stop_depth=10000, inc=5, units='ft'):
+def dog_leg_severity(md, inc, az):
+    """Assumes md, inc, and az are pandas series or numpy 1d array
+    Calculates Dog Leg Severity
+    Returns a numpy 1d array
+    """
+    # initialize an empty numpy array for Dog Leg Severity
+    dls = np.zeros(len(md))
+    # hard code the dog leg severity to be zero at the first sample
+    dls[0]= 0
+    # calculate measured depth increment, skipping the first sample
+    mdStep = np.diff(md[1:], n=1)
+    # create inc1, inc2 arrays offset by 1 index
+    inc1 = np.deg2rad(inc[0:-1])
+    inc2 = np.deg2rad(inc[1:])
+    # create az1, az2 arrays offset by 1 index
+    az1 = np.deg2rad(az[0:-1])
+    az2 = np.deg2rad(az[1:])
+    # compute dog leg severity
+    dls[1:] = (np.rad2deg(np.arccos((np.cos(inc1) * np.cos(inc2))
+    + (np.sin(inc1) * np.sin(inc2)) * np.cos(az2 - az1))) * (30 / mdStep))
+    return dls
+
+def reference_curves(dev_surv, start_depth=0, kb=32, stop_depth=10000, inc=0.5, units='m'):
     """This function builds uniformly spaced depth reference curves
     Assumes dev_surv a pandas dataframe
     Defaults start_depth, kb, stop_depth, inc, and units
-    Returns a pandas dataframe
+    Returns a pandas series
     """
     pass
 
